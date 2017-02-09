c9096625c40790ab38844067a492d26fc00682a8
Anh Lam <lilwiterose@yahoo.com>
2017-02-09 11:08:15 -1000

    wow feb 09

---
+++ addupper.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Xla on 2/9/17.
+ */
+
+function addupper(list){
+  var t= [];
+  console.log(t.toUpperCase());
+  return t;
+
+
+}
+addupper(["apple", "pear"]);
+addupper(["you", "can", "win"]);
